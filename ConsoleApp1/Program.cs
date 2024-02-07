45b9bd6bbb75236f3f6d4ac4f0085ee64879086d
GrigoryEbl <yatrahal.rus@mail.ru>
2024-02-07 20:38:52 +0400

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
@@ -14,27 +16,63 @@ class Program
         warehouse.Delive(iPhone11, 1);
 
         //Вывод всех товаров на складе с их остатком
+        shop.ShowGoodsInWarehouse();
 
-        Cart cart = shop.Cart();
-        cart.Add(iPhone12, 4);
-        cart.Add(iPhone11, 3); //при такой ситуации возникает ошибка так, как нет нужного количества товара на складе
+        Cart cart = shop.CreateCart();
+        cart.AddGood(iPhone12, 4, warehouse);
+        cart.AddGood(iPhone11, 3, warehouse); //при такой ситуации возникает ошибка так, как нет нужного количества товара на складе
 
         //Вывод всех товаров в корзине
+        cart.ShowGoodsInCart();
 
         Console.WriteLine(cart.Order().Paylink);
 
-        cart.Add(iPhone12, 9); //Ошибка, после заказа со склада убираются заказанные товары
+        cart.AddGood(iPhone12, 9, warehouse); //Ошибка, после заказа со склада убираются заказанные товары
     }
 }
 
 class Cart
 {
     private Dictionary<Good, uint> _goods = new Dictionary<Good, uint>();
+
     public IReadOnlyDictionary<Good, uint> Goods => _goods;
 
-    public void Add(Good item, uint count)
+    public event Action<IReadOnlyDictionary<Good, uint>> Ordered;
+
+    public void AddGood(Good good, uint count, Warehouse warehouse)
     {
-        _goods.Add(item, count);
+        if (warehouse.Goods.TryGetValue(good, out uint value))
+        {
+            if (value >= count)
+            {
+                _goods.Add(good, count);
+                Console.WriteLine($"Добавлен: {good.Name}: {count}шт.");
+            }
+            else
+                throw new InvalidOperationException("Превышено кол-во");
+        }
+    }
+
+    public void ShowGoodsInCart()
+    {
+        Console.WriteLine("Корзина");
+
+        foreach (var item in Goods)
+            Console.WriteLine(item.Key.Name + ": " + item.Value);
+
+        Console.WriteLine();
+    }
+
+    public Order Order()
+    {
+        if (Goods.Count == 0)
+        {
+            throw new InvalidOperationException("корзина пуста");
+        }
+
+        _goods.Clear();
+        Ordered?.Invoke(Goods);
+        return new Order("Ordered!");
     }
 }
 
@@ -42,20 +80,32 @@ class Warehouse
 {
     private Dictionary<Good, uint> _goods = new Dictionary<Good, uint>();
 
-    public void Delive(Good item, uint count)
+    public IReadOnlyDictionary<Good, uint> Goods => _goods;
+
+    public void Delive(Good good, uint count)
     {
-        _goods.Add(item, count);
+        _goods.Add(good, count);
+    }
+
+    public void RemoveGoods(IReadOnlyDictionary<Good, uint> goodsInCart)
+    {
+        
+
+        if (goodsInCart.ContainsKey() _goods.TryGetValue(, out uint value))
+        {
+            value -= count;
+        }
     }
 }
 
 class Good
 {
-    public string Name { get; private set; }
-
     public Good(string name)
     {
         Name = name;
     }
+
+    public string Name { get; private set; }
 }
 
 class Shop
@@ -68,8 +118,32 @@ class Shop
         _warehouse = warehouse;
     }
 
-    public Cart Cart()
+    public Cart CreateCart()
     {
+        _cart.Ordered += OnOrdered;
         return _cart = new Cart();
+    }
+
+    public void ShowGoodsInWarehouse()
+    {
+        Console.WriteLine("Склад");
+        foreach (var item in _warehouse.Goods)
+            Console.WriteLine(item.Key.Name + ": " + item.Value);
+        Console.WriteLine();
+    }
+
+    private void OnOrdered(IReadOnlyDictionary<Good, uint> goodsInCart)
+    {
+        _warehouse.RemoveGoods(goodsInCart);
+    }
+}
+
+class Order
+{
+    public readonly string Paylink;
+
+    public Order(string paylink)
+    {
+        Paylink = paylink;
     }
 }
